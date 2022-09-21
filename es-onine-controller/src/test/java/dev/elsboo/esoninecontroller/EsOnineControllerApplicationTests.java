f3ea153fffb717d11b96285170c4a36dda100995
AwesomeHye <45376682+AwesomeHye@users.noreply.github.com>
2022-09-21 21:27:56 +0900

    (20220921) 멀티모듈로 패키지 변경

---
+++ es-onine-controller/src/test/java/dev/elsboo/esoninecontroller/EsOnineControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elsboo.esoninecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsOnineControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
