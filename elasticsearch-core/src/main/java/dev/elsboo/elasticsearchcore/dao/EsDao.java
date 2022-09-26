4b8a168956fecf63a1eee9fa1bb41afa91dbbac1
AwesomeHye <45376682+AwesomeHye@users.noreply.github.com>
2022-09-26 23:35:39 +0900

    (20220926) 키바나 연동, ES java Client 인덱스 생성 시도

--- elasticsearch-core/src/main/java/dev/elsboo/elasticsearchcore/dao/EsDao.java
+++ elasticsearch-core/src/main/java/dev/elsboo/elasticsearchcore/dao/EsDao.java
@@ -1,17 +1,36 @@
 package dev.elsboo.elasticsearchcore.dao;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.mapping.TypeMapping;
+import co.elastic.clients.elasticsearch.indices.CreateIndexRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.client.indices.IndexTemplateMetadata;
+import org.elasticsearch.cluster.metadata.IndexMetadata;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.IndexSettings;
+import org.elasticsearch.xcontent.XContentBuilder;
+import org.elasticsearch.xcontent.XContentFactory;
+import org.elasticsearch.xcontent.XContentParser;
 import org.springframework.stereotype.Repository;
 
+import java.io.IOException;
+
 @Repository
+@RequiredArgsConstructor
 @Slf4j
 public class EsDao {
+    private final ElasticsearchClient client;
+
     public static void print() {
         log.info("PRINT");
     }
 
-    public void createIndex() {
-
+    public void createIndex(String indexName) throws IOException {
+        CreateIndexRequest createIndexRequest = new CreateIndexRequest.Builder()
+//                    .settings(new IndexSettings(IndexMetadata.builder("dd").settings(IndexTemplateMetadata.Builder.fromXContent(XContentFactory.jsonBuilder().field("ff")).).build()))
+                .build();
+        client.indices().create(createIndexRequest);
     }
 
 }
